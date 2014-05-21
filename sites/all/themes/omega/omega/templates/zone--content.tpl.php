da2b2aa4d06167b8d510f49dc5703177310643be
reut <vutzster@gmail.com>
2014-05-21 10:36:49 +0300

    V0.11

--- sites/all/themes/omega/omega/templates/zone--content.tpl.php
+++ sites/all/themes/omega/omega/templates/zone--content.tpl.php
@@ -1,11 +1,5 @@
-<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
-  <div<?php print $content_attributes; ?>>    
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb" class="grid-<?php print $columns; ?>"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>    
-    <?php if ($messages): ?>
-      <div id="messages" class="grid-<?php print $columns; ?>"><?php print $messages; ?></div>
-    <?php endif; ?>
+<?php if ($wrapper): ?><div <?php print $attributes; ?>><?php endif; ?>  
+  <div data="xxx" <?php print $content_attributes; ?>>    
     <?php print $content; ?>
   </div>
 <?php if ($wrapper): ?></div><?php endif; ?>
