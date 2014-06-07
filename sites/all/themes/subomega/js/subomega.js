8c39793f5a10b4408955afcfc9d8c9481c56ce0a
reut <vutzster@gmail.com>
2014-06-07 14:15:48 +0300

    halely v0.2 -- english

--- sites/all/themes/subomega/js/subomega.js
+++ sites/all/themes/subomega/js/subomega.js
@@ -1,25 +1,16 @@
-Drupal.avishay = {"normal":[],"fluid":[]};Drupal.avishay.isMSIE = /*@cc_on!@*/0;
+Drupal.avishay = {"normal":[],"fluid":[],"prevLayout":"null", "isMSIE" : /*@cc_on!@*/0};
+Drupal.avishay.BeforeCurrentLayoutReady = function(){
 
-jQuery(document).ready(function(){	
-//console.log("AFTER Drupal.avishay.ready");
 
 var menu = jQuery("#nice-menu-1"),
 	lang_switch = jQuery( ".language-switcher-locale-url li" );
 // ##  language switcher 
 jQuery('#en_switch').replaceWith(lang_switch.clone());
 jQuery("#block-menu-menu-footer .menu").append(lang_switch.clone());
+jQuery("#block-nodeblock-70 li:nth-child(8)").replaceWith(lang_switch.clone());
 menu.append( lang_switch);
 
-
-(function(){//## MOBILE	#	menu_toggle -- 
-var menu_toggle = function(){
-	jQuery("#menu_toggle").bind("click", function(e){
-				jQuery("#block-nice-menus-1 .content").toggle();	
-	});
-};
-Drupal.avishay.fluid.push(menu_toggle);
-})();
-(function(){// ## 	main search input behaviour
+// ## 	main search input behaviour
 var input = jQuery("#edit-search-block-form--2"),
 	lang = Drupal.settings.pathPrefix.replace(/\//,"");
 Drupal.avishay.search_text= {"en" : "search","he" : "חפשו"};
@@ -32,7 +23,86 @@ jQuery(input).on("click focus", function(e){
         jQuery(input).val("");
     }
 });
-})();   
+
+// ##  outbound links behaviour   
+jQuery('[href^=http]').attr("target","_blank");
+jQuery('.i18n-he.node-type-people #page-title').text("האנשים");
+jQuery('.i18n-en.node-type-people #page-title').text("People");
+
+};
+Drupal.avishay.AfterCurrentLayoutReady = function(){
+
+	// ## 	teams menu adition to people link
+	pepole_menu_head = jQuery(".menu-622:not(.people)");
+	pepole_menu_head.append(Drupal.avishay._get_teams()).addClass("people");
+	if(!jQuery("ul.hidden",pepole_menu_head).length){
+		pepole_menu_head.each(function(i, val){
+			jQuery(val).bind("click", function(e){
+				jQuery("ul", val).slideDown();
+				pepole_menu_head.unbind();			
+			});
+		});
+	};
+	//## # page_people || node_type_people
+	var page_people = jQuery("body").hasClass("page-people") ,
+		node_type_people = jQuery("body").hasClass("node-type-people"),
+		side_menu_block = jQuery('#block-block-6');
+	if(page_people || node_type_people){		jQuery(".menu-622").addClass("active-trail");		}
+	if( node_type_people){
+		var cat = jQuery("article .field-name-field-category a"),
+			tid = cat.attr("href").replace(/(.*)term\/(.*)/, '$2');
+			
+		jQuery("article .field-name-field-category a").replaceWith('<div data-tid="'+tid+'" >'+cat.text()+'</div>');
+		jQuery('[data-tid="'+tid+'"]').addClass("active").parent("li").addClass("active-trail");
+	}
+
+
+}
+Drupal.avishay._get_teams = function(){
+	if(Drupal.settings.teams){
+		var ul = jQuery("<ul></ul>");			
+		
+		jQuery(Drupal.settings.teams).each(function(i, val){
+			var href = '/'+Drupal.settings.pathPrefix+'people/'+val.tid;
+			var attr = {//"id": 'team_'+val.tid,
+						"href": href,
+						"data-tid": val.tid
+						};
+				// views - page people -- menu active stat
+			var regexp = new RegExp('/people/'+val.tid, 'g');
+			
+			var link = jQuery('<li></li>').append(jQuery('<a></a>').attr(attr).text(val.name));
+				
+			
+			if( window.location.pathname.match(regexp)){
+	
+				//console.log("window.location.pathname.match(regexp)");
+				jQuery(link).addClass("active-trail");
+				jQuery("a", link).addClass("active");
+				jQuery(ul).addClass("hidden");
+			}		
+			active = Drupal.avishay.teams;
+			
+			jQuery(ul).append(link);		
+		});					
+	return ul;
+	} else {
+	return false;
+	}
+};
+
+
+jQuery(document).ready(function(){	
+
+(function(){//## MOBILE	#	menu_toggle -- 
+var menu_toggle = function(){
+	jQuery("#menu_toggle").bind("click", function(e){
+				jQuery("#block-nice-menus-1 .content").toggle();	
+//				jQuery(e.currentTarget).toggleClass("red");
+	}).addClass("bind");
+};
+Drupal.avishay.fluid.push(menu_toggle);
+})();
 (function(){// ## 	side menu 
 var side_menu_block = function(){
 //	console.log("Drupal.avishay.normal.push(side_menu_block);");
@@ -67,105 +137,32 @@ Drupal.avishay.normal.push(side_menu_block);
 })();// ## 	END
 (function(){// ## 	touch menu behaviour	
 	var touch_menu_links_bind = function(){
-		jQuery(".menuparent a", menu).bind("click", menu_links_bind());
-	};	
-	Drupal.avishay.fluid.push(menu_links_bind);
-})();
-// ## 	END	
-Drupal.avishay._get_teams = function(){
-	if(Drupal.settings.teams){
-		var ul = jQuery("<ul></ul>");			
-		
-		jQuery(Drupal.settings.teams).each(function(i, val){
-			var href = '/people/'+val.tid,
-				link = jQuery('<li><a id="team_'
-								+val.tid+'" href="/" '
-								+'data-tid="'+val.tid+'" '
-								+Drupal.settings.pathPrefix.replace(/\//,"")
-								+href+'">'
-								+val.name+'</a></li>'),
-				active = Drupal.avishay.teams;
-			
-			jQuery(ul).append(link);		
-		});					
-	return ul;
-	} else {
-	return false;
-	}
-};
-Drupal.avishay.ready = function(){
-	
-//console.log("Drupal.avishay.ready => Drupal.avishay.timestamp ");
-
-// ##  outbound links behaviour   
-jQuery('[href^=http]').attr("target","_blank");
-jQuery('.i18n-he.node-type-people #page-title').text("האנשים");
-jQuery('.i18n-en.node-type-people #page-title').text("People");
-
-//## # page_people || node_type_people
-var page_people = jQuery("body").hasClass("page-people") ,
-	node_type_people = jQuery("body").hasClass("node-type-people"),
-	side_menu_block = jQuery('#block-block-6');
-if(page_people || node_type_people){		jQuery(".menu-622").addClass("active-trail");		}
-if( node_type_people){
-	var cat = jQuery("article .field-name-field-category a"),
-		tid = cat.attr("href").replace(/(.*)term\/(.*)/, '$2'),
-		people_menu = jQuery("");		
-	jQuery("article .field-name-field-category a").replaceWith('<div>'+cat.text()+'</div>');	
-	jQuery("#team_"+tid, people_menu).addClass("active").parent("li").addClass("active-trail");
-}
-
-(function(){// ## 	teams menu adition to people link
-
-pepole_menu_head = jQuery(".menu-622");
-pepole_menu_head.append(Drupal.avishay._get_teams());
-var hidden = true;
-
-jQuery("a", pepole_menu_head).each(function(i, val){
-	var tid = jQuery(val).attr("data-tid");
-	var href = '/people/'+tid;
-	var regexp = new RegExp(href, 'g');
-	if( window.location.pathname.match(regexp)){
-		jQuery("a", link).addClass("active").parent("li").addClass("active-trail");
-		hidden = false;
-	}
-});
-
-if(hidden){
-	pepole_menu_head.each(function(i, val){
-		jQuery(val).bind("click", function(e){
-			jQuery("ul", val).slideDown();
-			pepole_menu_head.unbind();			
+		jQuery(".menuparent a", menu).bind("click",function(e){
+			 menu_links_bind(e);
 		});
-	});
-};
-
+	};	
+	Drupal.avishay.fluid.push(touch_menu_links_bind);
 })();
-//##	END		## teams menu adition to people link ## 
-}
 
 jQuery(window).bind('resize', function (a) {
-	var CurrentLayout = Drupal.omega.getCurrentLayout();		
-	console.log("resize  TO => "+CurrentLayout);
-	if(CurrentLayout === "narrow"){
-		jQuery("body").addClass("responsive-layout-normal");
-	}
+	var CurrentLayout = Drupal.omega.getCurrentLayout();			
 	
-	jQuery(Drupal.avishay[CurrentLayout]).each(function(i, val){		
-		//console.log("resize  TO => "+CurrentLayout);			
-	//	console.log(a);
-		if(Drupal.avishay.timestamp !== a.timeStamp){			
-			val();
-			Drupal.avishay.ready();
-//			console.log("AFTER Drupal.avishay.ready");
-		}
+	if(CurrentLayout !== Drupal.avishay.prevLayout && Drupal.avishay.timestamp !== a.timeStamp){		
+		jQuery("body").addClass("responsive-layout-"+CurrentLayout).removeClass("responsive-layout-"+Drupal.avishay.prevLayout);
+		
+		Drupal.avishay.BeforeCurrentLayoutReady();
+		
+		jQuery(Drupal.avishay[CurrentLayout]).each(function(i, val){		
+			val();						
+		});
+		Drupal.avishay.AfterCurrentLayoutReady();
+		
+		
+		Drupal.avishay.prevLayout = CurrentLayout;
 		Drupal.avishay.timestamp = a.timeStamp;
-	});
+	};
 });
 jQuery(window).trigger('resize');	
-
-
-
 
 });// ## 	 jQuery document ready END
 
@@ -187,12 +184,13 @@ jQuery(window).trigger('resize');
         });*/
 function menu_links_bind(e){
 //				e.preventDefault();
-	
-	if(jQuery(e.currentTarget).hasClass("clicked") || !jQuery(e.currentTarget).parent("li").hasClass("menuparent") ){
-		return true;
-	}else { 
-		e.preventDefault();
-		jQuery(e.currentTarget).addClass("clicked");
-		return false;
+	if(typeof(e) !== "undefined"){
+		if(jQuery(e.currentTarget).hasClass("clicked") || !jQuery(e.currentTarget).parent("li").hasClass("menuparent") ){
+			return true;
+		}else { 
+			e.preventDefault();
+			jQuery(e.currentTarget).addClass("clicked");
+			return false;
+		}
 	}
 };
--- sites/all/themes/subomega/templates/region--menu.tpl.php
+++ sites/all/themes/subomega/templates/region--menu.tpl.php
@@ -1,7 +1,7 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
 <a id="home" href="/">home</a>    
-<span id="menu_toggle"></span>    
+<div id="menu_toggle"></div>    
 <?php print $content; ?>
   </div>
 </div>
