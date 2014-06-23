6486818459072f929445562fda885410f35121a4
reut <vutzster@gmail.com>
2014-06-23 12:01:54 +0300

    auto menu title

--- sites/all/modules/avihai/js/avihai.js
+++ sites/all/modules/avihai/js/avihai.js
@@ -1,18 +1,36 @@
+(function () {
+Drupal.behaviors.avihai = {
+	attach: function (context, settings) {
+		var menu_target = null,title = jQuery("#edit-title-field input");
+		
+			if(jQuery("form.node-form [name=changed]").val() !== ""){
+				title.attr("data-original-title", function(v,a){return this.value});
+				jQuery("#multiple-node-menu-wrapper input.form-text").each(function(i, val){
+					if(jQuery(val).val() === title.attr("data-original-title") ){
+						jQuery(val).attr("data-target",1);
+						menu_target = val;
+					}
+				});
+			}
+			title.bind("change blur keyup", function(e){
+				if(window.location.toString().match(/edit/) === null || window.location.toString().match(/edit(.*)add/) !== null){
+					jQuery("#edit-menu-link-title,#edit-multiple-node-menu-add-link-link-title").val(jQuery(e.currentTarget).val());
+				}else{
+					jQuery(menu_target).val(jQuery(e.currentTarget).val());
+				}
+			});
+			
+			jQuery("#edit-multiple-node-menu-enabled:not(:checked)").click();
+
+			}
+}
+})();
 
 jQuery(document).ready(function () { 
-	
-		jQuery("#edit-title-field input").bind("change blur keyup", function(e){
-			jQuery("#edit-menu-link-title").val(jQuery(e.currentTarget).val());
-		});
-	
-var t1= 	window.setTimeout(function(){
-
+	Drupal.behaviors.avihai.attach();
+	var t1 = window.setTimeout(function(){
 		if(jQuery("body").hasClass("admin-menu")){		
 			jQuery("#admin-menu [href*=\\/block\\/add]").attr("href",'/'+Drupal.settings.pathPrefix+"node/add/media-block"); 
 		}
-		
-
-
 	},1000);
-
 });
